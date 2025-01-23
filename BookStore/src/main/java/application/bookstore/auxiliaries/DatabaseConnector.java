0afccb05ecc7d34c1a0b73128710d295e18b757e
RegiLoshi <rloshi111@gmail.com>
2025-01-23 10:49:07 +0100

    Integration Testing

--- BookStore/src/main/java/application/bookstore/auxiliaries/DatabaseConnector.java
+++ BookStore/src/main/java/application/bookstore/auxiliaries/DatabaseConnector.java
@@ -3,14 +3,14 @@ package application.bookstore.auxiliaries;
 
 public interface DatabaseConnector {
 
-    String JDBC_URL = "jdbc:mysql://localhost/bookstore";
+//    String JDBC_URL = "jdbc:mysql://localhost/bookstore";
+//     String USER = "root";
+//     String PASSWORD = "workbench1213$$";
+
+
+     String JDBC_URL = "jdbc:mysql://localhost:3306/bookstore";
      String USER = "root";
-     String PASSWORD = "workbench1213$$";
-
-
-    // String JDBC_URL = "jdbc:mysql://localhost:3306/bookstore";
-     //String USER = "root";
-    // String PASSWORD = "11112003";
+     String PASSWORD = "11112003";
 
 
 
